0df48dd691d2b8eaad1a40d0ef65d795c90a9cc2
Isaac Thill <ithill22@turing.edu>
2023-06-12 11:08:19 -0600

    refactor book_search_facade

--- app/facades/book_search_facade.rb
+++ app/facades/book_search_facade.rb
@@ -10,31 +10,15 @@ class BookSearchFacade
 
   private
 
-  def service
-    _service ||= LibraryService.new
-  end
-
-  def forecast_service
-    _forecast_service ||= WeatherService.new
-  end
-
-  def map_service
-    _map_service ||= MapService.new
-  end
-  
-  def coordinates_data
-    _coordinates_data ||= map_service.get_coordinates(@location)
-  end
-
-  def coordinates
-    _coordinates ||= Coordinates.new(coordinates_data)
-  end
-
   def forecast_data
-    _forecast_data ||= forecast_service.get_forecast(coordinates.lat, coordinates.lng)
+    @forecast_data ||= WeatherService.new.get_forecast(coordinates.lat, coordinates.lng)
   end
 
   def book_data
-    _book_data ||= service.get_books(@location, @quantity)
+    @book_data ||= LibraryService.new.get_books(@location, @quantity)
+  end
+
+  def coordinates
+    @coordinates ||= Coordinates.new(MapService.new.get_coordinates(@location))
   end
 end
