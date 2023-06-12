e50bb3182a9031cbf1293432891dc082a5778dff
Isaac Thill <ithill22@turing.edu>
2023-06-12 09:15:34 -0600

    test: add request test for book-search

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :forecast, only: [:index]
       resources :users, only: [:create]
       resources :sessions, only: [:create]
+      resources :book_search, only: [:index]
     end
   end
 end
